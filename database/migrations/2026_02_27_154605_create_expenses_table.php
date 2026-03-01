9996fef21edd3b3b2c94091a9afb9426409777fd
WalidElmiloudi <elwalid38@gmail.com>
2026-03-01 01:27:02 +0000

    feat: create debts table with foreign key relationships and payment status

--- database/migrations/2026_02_27_154605_create_expenses_table.php
+++ database/migrations/2026_02_27_154605_create_expenses_table.php
@@ -13,25 +13,19 @@ return new class extends Migration
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
-
             $table->unsignedBigInteger('colocation_id');
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('category_id');
-
             $table->string('title');
             $table->decimal('amount', 10, 2);
             $table->date('expense_date');
-
             $table->timestamps();
-
             $table->foreign('colocation_id')
                   ->references('id')->on('colocations')
                   ->onDelete('cascade');
-
             $table->foreign('user_id')
                   ->references('id')->on('users')
                   ->onDelete('cascade');
-
             $table->foreign('category_id')
                   ->references('id')->on('categories')
                   ->onDelete('cascade');
